49f48c3ac714f03b46b2de3bf149b17a35133845
Hugo DEFOIS <defois.hugo@gmail.com>
2018-06-27 11:39:19 +0200

    [Compilation] Add structure

---
+++ src/fr/defois/modplus/Main.java
@@ -0,0 +1,37 @@
+package fr.defois.modplus;
+
+import fr.defois.modplus.Controller.RecordController;
+import fr.defois.modplus.Controller.SanctionApplyController;
+import fr.defois.modplus.Controller.SanctionController;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.IOException;
+
+public class Main extends JavaPlugin {
+
+    private ConfigManager _config;
+
+    @Override
+    public void onEnable() {
+        _config = new ConfigManager();
+        _config.setup();
+        getCommand("sanction").setExecutor(new SanctionController(_config));
+        getCommand("sanctionr").setExecutor(new SanctionApplyController(_config));
+        getCommand("record").setExecutor(new RecordController(_config));
+        loadConfig();
+    }
+
+    @Override
+    public void onDisable() {
+        try {
+            _config.saveSanctionsConfig();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void loadConfig() {
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+    }
+}
